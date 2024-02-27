382b528a5da659a4225336b5fb7b1583b0dc28bb
luthfi55 <luthfi12354@gmail.com>
2024-02-27 22:03:35 +0700

    Test Deployment

--- controllers/avtivateAccountController.go
+++ controllers/avtivateAccountController.go
@@ -9,6 +9,7 @@ import (
 	"fmt"
 	"math/rand"
 	"net/http"
+	"os"
 	"time"
 
 	"github.com/gin-gonic/gin"
@@ -352,7 +353,7 @@ func ListActivateDoctor(c *gin.Context) {
 	}
 
 	activationLink := "http://localhost:3000/api/user/activate/:user_id"
-	doctorresponse.GetInactiveDoctorSuccessResponse(c, "Get Data Successfully", response, activationLink, http.StatusOK)
+	doctorresponse.GetInactiveDoctorSuccessResponse(c, os.Getenv("GMAIL_SENDER"), response, activationLink, http.StatusOK)
 }
 
 func RejectDoctor(c *gin.Context) {
